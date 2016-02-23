b82f601c783a0fd3631c9a422f41b17f25f3b18e
Medic1de <sinpie29@hotmail.com>
2016-02-23 20:07:04 +0000

    added a basic 'register with the server' button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,12 +28,34 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form1";
+            this.btn_Register = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Register
+            // 
+            this.btn_Register.Location = new System.Drawing.Point(153, 86);
+            this.btn_Register.Name = "btn_Register";
+            this.btn_Register.Size = new System.Drawing.Size(242, 93);
+            this.btn_Register.TabIndex = 0;
+            this.btn_Register.Text = "Register with the server!";
+            this.btn_Register.UseVisualStyleBackColor = true;
+            this.btn_Register.Click += new System.EventHandler(this.btn_Register_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 444);
+            this.Controls.Add(this.btn_Register);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
 		}
 
-		#endregion
-	}
+        #endregion
+
+        private System.Windows.Forms.Button btn_Register;
+    }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -16,5 +18,35 @@ namespace ptpchat
 		{
 			InitializeComponent();
 		}
-	}
+
+        private void btn_Register_Click(object sender, EventArgs e)
+        {
+            var request = (HttpWebRequest)WebRequest.Create("http://1.2.3.4:9001/msg");
+
+            var postData = "msg_type=hello";
+            var data = Encoding.ASCII.GetBytes(postData);
+
+            request.Method = "POST";
+            request.ContentLength = data.Length;
+
+            try
+            {
+                using (var stream = request.GetRequestStream())
+                {
+                    stream.Write(data, 0, data.Length);
+                }
+            }
+            catch
+            {
+                //couldn't connect to the server, bail out and drop error messages
+                return;
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            //read response and do stuff with it
+        }
+    }
 }
