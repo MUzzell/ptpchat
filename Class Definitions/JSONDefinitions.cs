808b5f6ab914c0c88b9da2239c9d26b7ed18cbb0
Medic1de <sinpie29@hotmail.com>
2016-03-11 00:49:04 +0000

    fixed

---
+++ Class Definitions/JSONDefinitions.cs
@@ -0,0 +1,9 @@
+﻿namespace ptpchat.Class_Definitions
+{
+    internal static class JsonDefinitions
+    {
+        //public const string HelloJson = "\"msg_type\":\"HELLO\",\"msg_data\":\"{0}\"";
+
+        public const string HelloJson = "\"msg_type\":\"HELLO\",\"msg_data\":{{\"node_id\" : \"{0}\",\"version\" : \"{1}\"}}";
+    }
+}
---
+++ Communication Messages/HelloMessage.cs
@@ -0,0 +1,18 @@
+﻿namespace ptpchat.Communication_Messages
+{
+    using System;
+
+    using ptpchat.Class_Definitions;
+
+    internal class HelloMessage : BaseMessage
+    {
+        public new HelloData msg_data { get; set; }
+    }
+
+    internal class HelloData
+    {
+        public Guid node_id { get; set; }
+
+        public string version { get; set; }
+    }
+}
